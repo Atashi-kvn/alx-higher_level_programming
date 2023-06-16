2f4734de3333aeb210b014e388d3dc44943d6ba3
Atash-kv <ataskv.gov@gmail.com>
2023-06-16 05:54:56 +0300

    update

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -8,43 +8,43 @@
  **/
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *aux_node = *head;
+	dlistint_t *current_node = *head;
 	dlistint_t *node_to_delete = *head;
-	unsigned int idx;
-	unsigned int cont = 0;
+	unsigned int current_index;
+	unsigned int current_count = 0;
 
-	/* border case for empty list */
+	/* Border case: empty list */
 	if (!(*head))
-		return (-1);
+		return -1;
 
-	/* border case for delete at the beginning */
+	/* Border case: delete at the beginning */
 	if (index == 0)
 	{
 		*head = node_to_delete->next;
 		free(node_to_delete);
 		if (*head)
 			(*head)->prev = NULL;
-		return (1);
+		return 1;
 	}
 
-	/* search of position to delete */
-	idx = index - 1;
-	while (aux_node && cont != idx)
+	/* Find the position to delete */
+	current_index = index - 1;
+	while (current_node && current_count != current_index)
 	{
-		cont++;
-		aux_node = aux_node->next;
+		current_count++;
+		current_node = current_node->next;
 	}
 
-	/* general case */
-	if (cont == idx && aux_node)
+	/* General case */
+	if (current_count == current_index && current_node)
 	{
-		node_to_delete = aux_node->next;
+		node_to_delete = current_node->next;
 		if (node_to_delete->next)
-		node_to_delete->next->prev = aux_node;
-		aux_node->next = node_to_delete->next;
+			node_to_delete->next->prev = current_node;
+		current_node->next = node_to_delete->next;
 		free(node_to_delete);
-		return (1);
+		return 1;
 	}
 
-	return (-1);
+	return -1;
 }
