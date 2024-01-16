1949ab8c40da51ec218aa5029b688e67c43e88a9
Atandi Kelvin <ataskv.gov@gmail.com>
2024-01-16 04:15:29 +0300

    Updates task restart

--- 0x0B-python-input_output/0-read_file.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Defines a text file reading function."""
-
-
-def read_file(filename=""):
-    """Will print the content of a UTF8 file."""
-    with open(filename, encoding="utf-8") as file:
-        print(file.read(), end="")
